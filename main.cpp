7570d89d884c8241ab7b9f5274e31d4b63b57a5a
Tristan <simontristandev@gmail.com>
2026-06-01 18:15:17 +0200

    add all in 1 file

--- main.cpp
+++ main.cpp
@@ -1,23 +1,18 @@
 
 #include <iostream>
 #include "src/Chroma.hpp"
-#include "src/colors.hpp"
-#include "src/flags.hpp"
 
 #ifdef _WIN32 
 #include <windows.h>
 #endif
 
 int main() {
-    Chroma::Settings |= PRINT_BOLD | PRINT_BLINKING;
+    Chroma::Settings |= PRINT_BOLD | PRINT_BLINKING | PRINT_TIME;
     Chroma::Init();
     //std::cout << LogLib::settings << std::endl;#
 
     Chroma::print_success("%s %s\n", "hi", "lol");
-    Chroma::remove_setting(PRINT_BOLD);
-    Chroma::remove_setting(PRINT_BLINKING);
-    Chroma::print_color("%s\n", COLORS::BLUE, "hi");
-
     std::cin.get();
 
+    return 0;
 }
--- src/Chroma.hpp
+++ src/Chroma.hpp
@@ -3,101 +3,143 @@
 #include <cstdio>
 #include <cstring>
 #include <stdio.h>
-#include "./colors.hpp"
-#include "./flags.hpp"
 
-#ifdef _WIN32 
+#ifdef _WIN32
 #include <Windows.h>
 #endif
-//1mb stacksize windows
-//8mb stacksize linux
-//https://lists.gnu.org/archive/html/bug-coreutils/2009-10/msg00262.html
-
+// 1mb stacksize windows
+// 8mb stacksize linux
+// https://lists.gnu.org/archive/html/bug-coreutils/2009-10/msg00262.html
 
 namespace Chroma {
-    inline int Settings = 0;
+inline int Settings = 0;
 
-    inline void Init() {
-    #ifdef _WIN32 
-    // set console mode to ansii
-    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE); //get output handle
-    DWORD dwMode = 0;
-    GetConsoleMode(hOut, &dwMode); //get the current conole handle
-    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING; //set the bits in DWMODE to activate ANSI/VT100
-    SetConsoleMode(hOut, dwMode); // set the new mode
-    #endif
-    }
-
-    inline char* add_setting(char* format) {
-        uint64_t color_code_base_address = reinterpret_cast<uint64_t>(format);
-
-        if (Settings&PRINT_BOLD) { //code ;1 (1<<0)
-            while ((*format) != 0x6D) format++;
-
-            *(format) = 0x3B;
-            *(format+1) = 0x31;
-            *(format+2) = 0x6D;
-        }
-
-        if (Settings&PRINT_BLINKING) { //code ;1 (1<<0)
-            while ((*format) != 0x6D) format++;
-
-            *(format) = 0x3B;
-            *(format+1) = 0x35;
-            *(format+2) = 0x6D;
-        }
-
-        return (char*)color_code_base_address;
-    };
-
-    inline void remove_setting(int settings_to_remove, ...) {
-        if (Settings&settings_to_remove) {
-            Settings = Settings & ~(settings_to_remove); 
-        }
-    }
-    
-    
-    template <typename ... Types>
-    inline void print_success(const char* format, Types... args) {
-        char buffer[256];
-        std::strcpy(buffer, add_setting(COLORS::GREEN));
-        std::strcat(buffer, format);
-        std::strcat(buffer, COLORS::RESET);
-
-        printf(buffer, args...);
-    };
-
-    template <typename ... Types>
-    inline void print_warning(const char* format, Types... args) {
-        char buffer[256];
-        std::strcpy(buffer, add_setting(COLORS::YELLOW));
-        std::strcat(buffer, format);
-        std::strcat(buffer, COLORS::RESET);
-
-        printf(buffer, args...);
-    };
-
-    template <typename ... Types>
-    inline void print_error(const char* format, Types... args) {
-        char buffer[256];
-        std::strcpy(buffer, add_setting(COLORS::RED));
-        std::strcat(buffer, format);
-        std::strcat(buffer, COLORS::RESET);
-
-        printf(buffer, args...);
-    };
-
-    template <typename ... Types>
-    inline void print_color(const char* format, char* color, Types... args) {
-        char buffer[256];
-        std::strcpy(buffer, add_setting(color));
-        std::strcat(buffer, format);
-        std::strcat(buffer, COLORS::RESET);
-
-        printf(buffer, args...);
-    }
-
-    
-
-
+inline void Init() {
+#ifdef _WIN32
+  // set console mode to ansii
+  HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE); // get output handle
+  DWORD dwMode = 0;
+  GetConsoleMode(hOut, &dwMode);                // get the current conole handle
+  dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING; // set the bits in DWMODE to
+                                                // activate ANSI/VT100
+  SetConsoleMode(hOut, dwMode);                 // set the new mode
+#endif
 }
+
+namespace ChromaFlags {
+    #define PRINT_BOLD (1<<0) //bit 1
+    #define PRINT_TIME (1<<1) //bit 2
+    #define PRINT_FILE (1<<2) //bit 3
+    #define PRINT_BLINKING (1<<3) //bit 4
+}
+
+namespace ChromaColors {
+    inline char RESET[32] =  "\033[0m";
+    inline char BLACK[32] = "\033[30m";      /* Black */
+    inline char RED[32] =   "\033[31m";      /* Red */
+    inline char GREEN[32] = "\033[32m";      /* Green */
+    inline char YELLOW[32] ="\033[33m";      /* Yellow */
+    inline char BLUE[32] =  "\033[34m";      /* Blue */
+    inline char MAGENTA[32]="\033[35m";      /* Magenta */
+    inline char CYAN[32] =  "\033[36m";      /* Cyan */
+    inline char WHITE[32] = "\033[37m";      /* White */
+    inline char BOLDBLACK[32] =   "\033[1m\033[30m";      /* Bold Black */
+    inline char BOLDRED[32] =     "\033[1m\033[31m";      /* Bold Red */
+    inline char BOLDGREEN[32] =   "\033[1m\033[32m";      /* Bold Green */
+    inline char BOLDYELLOW[32] =  "\033[1m\033[33m";      /* Bold Yellow */
+    inline char BOLDBLUE[32] =    "\033[1m\033[34m";      /* Bold Blue */
+    inline char BOLDMAGENTA[32] = "\033[1m\033[35m";      /* Bold Magenta */
+    inline char BOLDCYAN[32] =    "\033[1m\033[36m";      /* Bold Cyan */
+    inline char BOLDWHITE[32] =   "\033[1m\033[37m";      /* Bold White */
+}
+
+namespace ChromaInternalFunctions {
+
+static inline char *add_ansi_settings(char *ansi_color_buffer) {
+  uint64_t color_code_base_address =
+      reinterpret_cast<uint64_t>(ansi_color_buffer);
+
+  if (Settings & PRINT_BOLD) { // code ;1 (1<<0)
+    while ((*ansi_color_buffer) != 0x6D)
+      ansi_color_buffer++;
+
+    *(ansi_color_buffer) = 0x3B;
+    *(ansi_color_buffer + 1) = 0x31;
+    *(ansi_color_buffer + 2) = 0x6D;
+  }
+
+  if (Settings & PRINT_BLINKING) { // code ;1 (1<<0)
+    while ((*ansi_color_buffer) != 0x6D)
+      ansi_color_buffer++;
+
+    *(ansi_color_buffer) = 0x3B;
+    *(ansi_color_buffer + 1) = 0x35;
+    *(ansi_color_buffer + 2) = 0x6D;
+  }
+
+  return (char *)color_code_base_address;
+};
+
+inline void add_time(char *buffer) {
+  if (Settings & PRINT_TIME) {
+    std::strcat(buffer, " [] ");
+  }
+}
+
+inline void add_current_file(char *buffer) {
+  if (Settings & PRINT_FILE) {
+    std::strcat(buffer, "test");
+  }
+}
+} // namespace InternalFunctions
+
+inline void remove_setting(int settings_to_remove, ...) {
+  if (Settings & settings_to_remove) {
+    Settings = Settings & ~(settings_to_remove);
+  }
+}
+
+
+
+template <typename... Types>
+inline void print_success(const char *format, Types... args) {
+  char buffer[256];
+  std::strcpy(buffer, ChromaInternalFunctions::add_ansi_settings(COLORS::GREEN));
+  ChromaInternalFunctions::add_time(buffer);
+  std::strcat(buffer, format);
+  std::strcat(buffer, COLORS::RESET);
+
+  printf(buffer, args...);
+};
+
+template <typename... Types>
+inline void print_warning(const char *format, Types... args) {
+  char buffer[256];
+  std::strcpy(buffer, ChromaInternalFunctions::add_ansi_settings(COLORS::YELLOW));
+  std::strcat(buffer, format);
+  std::strcat(buffer, COLORS::RESET);
+
+  printf(buffer, args...);
+};
+
+template <typename... Types>
+inline void print_error(const char *format, Types... args) {
+  char buffer[256];
+  std::strcpy(buffer, ChromaInternalFunctions::add_ansi_settings(COLORS::RED));
+  std::strcat(buffer, format);
+  std::strcat(buffer, COLORS::RESET);
+
+  printf(buffer, args...);
+};
+
+template <typename... Types>
+inline void print_color(const char *format, char *color, Types... args) {
+  char buffer[256];
+  std::strcpy(buffer, ChromaInternalFunctions::add_ansi_settings(color));
+  std::strcat(buffer, format);
+  std::strcat(buffer, COLORS::RESET);
+
+  printf(buffer, args...);
+}
+
+} // namespace Chroma
