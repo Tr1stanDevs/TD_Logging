5f70d42dd95060b8bdf26b8101cf3b0db2c0b7b2
Tristan <simontristandev@gmail.com>
2026-06-10 13:49:13 +0200

    fix a bug in ps script and bump version up

--- test-windows.ps1
+++ test-windows.ps1
@@ -1,11 +1,11 @@
 
 $build_type = "Release"
 
-if (Test-Path build) {
-    Remove-Item build -Recurse -Force
+if (Test-Path .\build) {
+    Remove-Item .\build -Recurse -Force
 }
 
-Remove-Item -Path build -Recurse -Force
 cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 cmake --build build --config $build_type
+Clear-Host
 .\build\Release\TD_Logging.exe
